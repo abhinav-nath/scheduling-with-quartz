3beb68a1430a4c6e13072a0ca855d6454654b71c
Abhinav Nath <abhinavnath@ymail.com>
2022-02-02 23:43:06 +0530

    Initialise project

---
+++ src/main/java/com/codecafe/scheduling/SchedulingWithQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingWithQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingWithQuartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecafe/scheduling/SchedulingWithQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecafe.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingWithQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
