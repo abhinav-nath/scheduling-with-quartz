12a48c6d539ee40d7fbfad712bed83e7a66509e2
Abhinav Nath <abhinavnath@ymail.com>
2022-02-08 00:19:00 +0530

    Use JobExplorer to get last completed job details

--- src/main/java/com/codecafe/scheduling/config/BatchConfiguration.java
+++ src/main/java/com/codecafe/scheduling/config/BatchConfiguration.java
@@ -8,9 +8,11 @@ import com.codecafe.scheduling.reader.SourceProductsReader;
 import com.codecafe.scheduling.writer.TargetProductsWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +33,13 @@ public class BatchConfiguration {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
     }
 
     @Bean
--- src/main/java/com/codecafe/scheduling/reader/SourceProductsReader.java
+++ src/main/java/com/codecafe/scheduling/reader/SourceProductsReader.java
@@ -4,10 +4,7 @@ import com.codecafe.scheduling.entity.SourceProduct;
 import com.codecafe.scheduling.repository.SourceProductsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +12,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class SourceProductsReader implements ItemReader<SourceProduct> {
 
     private int nextProductIndex;
-    private JobExecution jobExecution;
     private List<SourceProduct> sourceProducts;
 
     private final SourceProductsRepository sourceProductsRepository;
@@ -34,28 +31,17 @@ public class SourceProductsReader implements ItemReader<SourceProduct> {
         this.sourceProductsRepository = sourceProductsRepository;
     }
 
-    @BeforeStep
-    public void beforeStep(StepExecution stepExecution) {
-        jobExecution = stepExecution.getJobExecution();
-    }
-
     @Override
     public SourceProduct read() {
         log.info("==> Entered inside SourceProductsReader::read method");
 
-        Date lastSuccessfulJobRun = new Date();
-        long instanceId = jobExplorer.getLastJobInstance("importProductsJob").getInstanceId();
+        Optional<Date> lastCompletedJobTime = getLastCompletedJobTime();
 
-        System.out.println("######## Last Job Status : " + jobExplorer.getLastJobExecution(jobExplorer.getJobInstance(instanceId)).getStatus());
+        if (lastCompletedJobTime.isPresent())
+            sourceProducts = sourceProductsRepository.findByLastSuccessfulJobRun(lastCompletedJobTime.get());
+        else
+            sourceProducts = sourceProductsRepository.findAll();
 
-        if (BatchStatus.COMPLETED == jobExplorer.getLastJobExecution(jobExplorer.getJobInstance(instanceId)).getStatus()) {
-            System.out.println("*********************** YOOOOO *************************");
-            lastSuccessfulJobRun = jobExplorer.getLastJobExecution(jobExplorer.getJobInstance(instanceId)).getEndTime();
-        }
-
-        log.info("Last successful job run was at [{}]", lastSuccessfulJobRun);
-
-        sourceProducts = sourceProductsRepository.findByLastSuccessfulJobRun(lastSuccessfulJobRun);
         SourceProduct nextProduct = null;
 
         if (nextProductIndex < sourceProducts.size()) {
@@ -67,6 +53,22 @@ public class SourceProductsReader implements ItemReader<SourceProduct> {
 
         log.info("<== Exiting from SourceProductsReader::read method with source product [{}]", nextProduct != null ? nextProduct.getName() : null);
         return nextProduct;
+    }
+
+    private Optional<Date> getLastCompletedJobTime() {
+        Optional<Date> lastCompletedJobTime = Optional.empty();
+        List<JobInstance> jobInstances = jobExplorer.getJobInstances("importProductsJob", 0, 10);
+
+        Optional<JobInstance> lastCompletedJobInstanceOptional = jobInstances.stream().filter(jobInstance -> {
+            long instanceId = jobInstance.getInstanceId();
+            return BatchStatus.COMPLETED == jobExplorer.getJobExecution(instanceId).getStatus();
+        }).findFirst();
+
+        if (lastCompletedJobInstanceOptional.isPresent()) {
+            log.info("######## Last Job [{}] status: [{}]", lastCompletedJobInstanceOptional.get().getInstanceId(), jobExplorer.getJobExecution(lastCompletedJobInstanceOptional.get().getInstanceId()).getStatus());
+            lastCompletedJobTime = Optional.of(jobExplorer.getLastJobExecution(jobExplorer.getJobInstance(lastCompletedJobInstanceOptional.get().getInstanceId())).getEndTime());
+        }
+        return lastCompletedJobTime;
     }
 
 }
